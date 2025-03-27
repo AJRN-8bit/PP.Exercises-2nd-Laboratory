6ab5403d03e14266e3389a01a154bf5a48a90a59
AJRN-8bit <azairodrivarro@gmail.com>
2025-03-26 20:07:03 -0600

    Update Exercise40.py

--- Part3 (37-40)/Exercise40.py
+++ Part3 (37-40)/Exercise40.py
@@ -1,43 +1,45 @@
-# Stores the word and its lenght in a dictionary as key-value.
+# Stores the words of a paragraph by word size in dictionary as key-set.
 
 def WordLenght():
-    print("------------------------------------------------------------")
-    print("Stores the word and its lenght in a dictionary as key-value.")
-    print("------------------------------------------------------------")
+    print("----------------------------------------------------------------------")
+    print("Stores the words of a paragraph by word size in dictionary as key-set.")
+    print("----------------------------------------------------------------------")
     print()
 
     try:
-        # Saves the amount of words to evaluate.
-        wordsAmount = int(input("Insert the number of words to register (interger): "))
+        # Saves an entered paragraph.
+        paragraph = input("Enter a paragraph: ")
         print()
 
-        word_length = {}  # Dictionary initialization.
-
-        print("Enter word: ")  # Message
-        print()
+        word_length_set = {}  # Dictionary initialization.
  
-        words = []  # List initialization.
+        words = paragraph.split()  # Converts the paragraph in a list of words.
 
-        for x in range(1, wordsAmount +1):  # It asks the word the amount of times requestes. 
-            word = input(f"Word {x}: ")  # Saves the name and grade as list.
-            words.append(word)  # Adds the word to the words list.
+        words = [word.lower() for word in words]  # Converts all the words to lowercase.
+
+        # Removes the special characters from the paragraph. Help from AI.
+        words = ''.join(char.lower() if char.isalnum() or char.isspace() else ' ' for char in paragraph).split()
 
 
-        for word in words:  # Make a loop for every word in the word list.
+        for word in words:  # Iterates over the list of words.
+             
+            if len(word) in word_length_set:
+                # Takes the length of the word as key and adds a word to the set if it matches the key.
+                # Creates an automatic set.
+                word_length_set[len(word)].add(word)  
 
-            lenght = ""  # Lenght initialization.
-            for n in range(1, len(word)+1):  # It makes the loop for the lenght of the word.
-                x = str(n)  # Converts the current number loop into a string.
-                lenght += x  # Adds the number to the empty string.
+            else: # If the lenght key doesnt exists, it adds a new key an the current word.
+                word_length_set.update({len(word): {word}})
 
-            word_length.update({word: lenght})  # Adds the key and value to the dictionary.
+
+        word_length_set = dict(sorted(word_length_set.items(), reverse= False))  # Sorts the dictionary keys in ascending order.
 
         print()
-        print("Create dictionary: ")  # Message
+        print("Created dictionary: ")  # Message
         print()
 
-        for key, value in word_length.items():  # Prints all the keys and its values to view them in a better way.
-                print(f"{key}: {value}")
+        for key, _set in word_length_set.items():  # Prints all the keys and its values to view them in a better way.
+                print(f"{key}: {_set}")
 
 
     except Exception as e:
